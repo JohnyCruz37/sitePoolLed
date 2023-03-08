711e70f7a104a5cf2cc69e4b553ef425418e0a8d
JohnyCruz37 <johnygoncalvescruz@gmail.com>
2023-03-08 17:26:35 -0300

    Criando os atomos do site - EM ANDAMENTO

---
+++ site_poolled/src/Paginas/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Botao from "../Atomos/botaoAcao/Botao";
+
+const Home = () => {
+    return (
+        <div>
+            <Botao texto="Orçamento"/>
+        </div>
+    )
+}
+
+export default Home;
--- site_poolled/src/main.jsx
+++ site_poolled/src/main.jsx
@@ -1,10 +1,22 @@
+// DEFINIÇÃO DE ROTAS
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 import App from './App'
+import Home from "./Paginas/Home"
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App/>}>
+          <Route path='/' element={<Home/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
