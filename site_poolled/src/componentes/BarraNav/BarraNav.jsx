709ce57e4039ce3e036995769e88b468fb76f50c
JohnyCruz37 <johnygoncalvescruz@gmail.com>
2023-03-09 17:33:44 -0300

    finalizado o menu (mobile)

--- site_poolled/src/componentes/BarraNav/BarraNav.jsx
+++ site_poolled/src/componentes/BarraNav/BarraNav.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import './BarraNav.css'
-import { Navbar, Nav, Container, NavDropdown, Button } from "react-bootstrap"
+import { Navbar, Nav, Container, } from "react-bootstrap"
 
 import logoMenu from "../../img/logo.png"
+
+import Botao from "../../Atomos/botaoAcao/Botao"
+
+import './BarraNav.css'
 
 const BarraNav = () => {
 
     return (
         <>
-            <Navbar collapseOnSelect expand="md" bg="light">
-                <Container>
+            <Navbar collapseOnSelect expand="lg" className="barra-nav">
+                <Container className="container-nav">
                     <Navbar.Brand href="#">
-                        <img src={logoMenu} alt="logo da empresa" width="80" height="80" />
+                        <img src={logoMenu} alt="logo da empresa" width="80" height="80" className="img-logo" />
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav" >
-                        <Nav className="me-auto me-2 me-lg-0">
-                            <Link to="/"><Button> <h5> home </h5> </Button></Link>
-                            <Link to="/sobre"><Button> <h5> sobre </h5> </Button></Link>
-                            <Link to="/servicos"><Button> <h5> serviços </h5> </Button></Link>
-                            <Link to="#"><Button> <h5> produtos </h5> </Button></Link>
-                            <Link to="/contato"><Button> <h5> contato </h5> </Button></Link>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" className="nav-icone" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="barra-nav-itens">
+                            <Nav.Link eventKey="/" className="barra-nav-item" to="/"> <Link to="/"> <h5 className="texto-link"> home </h5> </Link>  </Nav.Link>
+                            <Nav.Link eventKey="/sobre" className="barra-nav-item" to="/sobre"> <Link to="/sobre"> <h5 className="texto-link"> sobre </h5> </Link> </Nav.Link>
+                            <Nav.Link eventKey="/servicos" className="barra-nav-item" to="/servicos"> <Link to="/servicos"> <h5 className="texto-link"> serviços </h5> </Link> </Nav.Link>
+                            <Nav.Link eventKey="/galeria" className="barra-nav-item" to="/galeria"> <Link to="/galeria"> <h5 className="texto-link"> galeria </h5> </Link> </Nav.Link>
+                            <Nav.Link eventKey="/contato" className="barra-nav-item" to="/contato"> <Link to="/contato"> <h5 className="texto-link"> contato </h5> </Link> </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
+                    <Container className="btn-orcamento">
+                        <Botao texto="Orçamento" />
+                    </Container>
                 </Container>
             </Navbar>
         </>
--- site_poolled/src/componentes/Header/Header.jsx
+++ site_poolled/src/componentes/Header/Header.jsx
@@ -6,9 +6,9 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <BarraNav/>
-        </div>
+        <header className="topo">
+            <BarraNav />
+        </header>
     )
 }
 
